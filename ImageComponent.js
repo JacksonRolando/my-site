f8f664f5ea449f42b5fd18e909d70ffb282ed462
Jackson Rolando <jprolando7762@gmail.com>
2026-02-03 16:08:14 -0800

    time to save some work

---
+++ ImageComponent.js
@@ -0,0 +1,5 @@
+const ImageComponent = (link, title, caption) => {
+    return `
+        
+    `
+}
---
+++ control.js
@@ -0,0 +1,44 @@
+const radiansRadiusToPos = (radians, remRadius) => {
+    let degreeRadius = remRadius * parseFloat(getComputedStyle(document.documentElement).fontSize);
+    let x = degreeRadius * Math.cos(radians)
+    let y = degreeRadius * Math.sin(radians)
+    return { x, y }
+}
+
+const circleSplit = (numElementsToAdd, offset = 0) => {
+    let radiansList = []
+    let increment = 2 * Math.PI / numElementsToAdd
+
+    for (let i = 0; i < numElementsToAdd; i++) {
+        radiansList.push(offset + increment * i)
+    }
+
+    return radiansList
+}
+
+const fanStack = (stackElementId, offsetRadians = 0, remRadius = 10) => {
+    const children = document.getElementById(stackElementId).children
+
+    const radiansList = circleSplit(children.length - 1, offsetRadians)
+    const posList = []
+
+    radiansList.forEach(radian => {
+        posList.push(radiansRadiusToPos(radian, remRadius))
+    });
+
+    for (let i = 1; i < children.length; i++) {
+        let child = children[i].children[0]
+        child.style.translate = `calc(-50% - ${posList[i - 1].x}px) ${posList[i - 1].y}px`
+        child.classList.add('under-stack-shown')
+    }
+
+    window.addEventListener('click', function (e) {
+        if (!document.getElementById(stackElementId).contains(e.target)) {
+            for (let i = 1; i < children.length; i++) {
+                let child = children[i].children[0]
+                child.classList.remove('under-stack-shown')
+            }
+        }
+    });
+
+}
