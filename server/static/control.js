a8e203477e2aacde363986b72ca084c0ec4855e0
Jackson Rolando <jprolando7762@gmail.com>
2026-02-03 23:48:08 -0800

    fix center, convert to a

--- server/static/control.js
+++ server/static/control.js
@@ -1,7 +1,7 @@
 const radiansRadiusToPos = (radians, remRadius) => {
     let degreeRadius = remRadius * parseFloat(getComputedStyle(document.documentElement).fontSize);
-    let x = degreeRadius * Math.cos(radians)
-    let y = degreeRadius * Math.sin(radians)
+    let x = Math.round(degreeRadius * Math.cos(radians))
+    let y = Math.round(degreeRadius * Math.sin(radians))
     return { x, y }
 }
 
@@ -29,14 +29,14 @@ const fanStack = (stackElementId, offsetRadians = 0, remRadius = 10) => {
     for (let i = 1; i < children.length; i++) {
         let child = children[i].children[0]
         child.style.translate = `calc(-50% - ${posList[i - 1].x}px) ${posList[i - 1].y}px`
-        child.classList.add('under-stack-shown')
+        child.classList.add('stack-card-under-shown')
     }
 
     window.addEventListener('click', function (e) {
         if (!document.getElementById(stackElementId).contains(e.target)) {
             for (let i = 1; i < children.length; i++) {
                 let child = children[i].children[0]
-                child.classList.remove('under-stack-shown')
+                child.classList.remove('stack-card-under-shown')
             }
         }
     });
